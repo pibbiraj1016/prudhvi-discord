38039b4fd5cb7e9d3734693b909f8c815ea5d8b9
Prudhvi Raj <pibbi@Pibbis-Air.lan>
2024-02-24 18:55:16 -0500

    final

---
+++ app/api/livekit/route.ts
@@ -0,0 +1,30 @@
+import { AccessToken } from "livekit-server-sdk";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const room = req.nextUrl.searchParams.get("room");
+  const username = req.nextUrl.searchParams.get("username");
+  if (!room) {
+    return NextResponse.json({ error: 'Missing "room" query parameter' }, { status: 400 });
+  } else if (!username) {
+    return NextResponse.json({ error: 'Missing "username" query parameter' }, { status: 400 });
+  }
+
+  const apiKey = process.env.LIVEKIT_API_KEY;
+  const apiSecret = process.env.LIVEKIT_API_SECRET;
+  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+
+  if (!apiKey || !apiSecret || !wsUrl) {
+    return NextResponse.json({ error: "Server misconfigured" }, { status: 500 });
+  }
+
+  const at = new AccessToken(apiKey, apiSecret, { identity: username });
+
+  // Add grants and wait for them to be added
+  await at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+
+  // Convert access token to JWT after grants have been added
+  const token = at.toJwt();
+
+  return NextResponse.json({ token });
+}
